fe3b4893adac178af6bba51d5773f694efb9cf28
blacktop <blacktop@users.noreply.github.com>
2017-09-10 16:58:30 -0600

    add a mk file

---
+++ golang.mk
@@ -0,0 +1,23 @@
+include github.com/tj/make/cloc
+include github.com/tj/make/todo
+include github.com/tj/make/static/stats
+
+# Run all tests.
+test:
+	@go test -cover ./...
+.PHONY: test
+
+# Install the commands.
+install:
+	@go install ./cmd/...
+.PHONY: install
+
+# Release binaries to GitHub.
+release:
+	@goreleaser --rm-dist --config .goreleaser.yml
+.PHONY: release
+
+# Show size of imports.
+size:
+	@curl -sL https://gist.githubusercontent.com/tj/04e0965e23da00ca33f101e5b2ed4ed4/raw/9aa16698b2bc606cf911219ea540972edef05c4b/gistfile1.txt | bash
+.PHONY: size
